86a6244bec2c7fe5c5fe5500080938c39b13d2ba
eazycchi <mochizza123@gmail.com>
2020-12-16 00:55:28 +0700

    menambahkan view dashboard sama dummy login

---
+++ app/Views/pages/login.php
@@ -0,0 +1,12 @@
+<?= $this->extend('layout/template');?>
+
+<?= $this->section('content');?>
+<?= $this->include('layout/navbarAwal')?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            Isinya halaman login
+        </div>
+    </div>
+</div>
+<?= $this->endSection(); ?>
