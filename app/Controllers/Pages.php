1ffe7d0df2490e73c331772e491a50fa5eb42a3e
Xeryus <akhmadfadilm@gmail.com>
2020-12-24 09:30:53 +0700

    pages controller tidak digunakan

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -1,19 +1,117 @@
-<?php namespace App\Controllers;
+<!-- tidak digunakan -->
 
-use App\Models\UserModel;
+<!-- <php
+
+namespace App\Controllers;
+
+// use App\Models\UserModel;
 
 class Pages extends BaseController
 {
-	protected $userModel;
+	// protected $userModel;
 
-	public function __construct()
-	{
-		$this->userModel = new UserModel();
-	}
+	// public function __construct()
+	// {
+	// 	$this->userModel = new UserModel();
+	// }
+
 	public function index()
 	{
-        $data = [
-            'title' => 'Dashboard | Riset 5 Website Alumni',
+		if (isset($_REQUEST['code']) && $_REQUEST['code']) {
+
+			$this->modelAuth = new \App\Models\AuthModel();
+
+			$curl_status = curl_init();
+
+			curl_setopt_array($curl_status, [
+				CURLOPT_RETURNTRANSFER => 1,
+				CURLOPT_URL => 'https://ws.stis.ac.id/oauth/token',
+				CURLOPT_POST => 1,
+
+				CURLOPT_POSTFIELDS => [
+					'grant_type' => 'authorization_code',
+					'client_id' => '14',
+					'client_secret' => '3r3grLcMKEEqhq1gHbks1ZzztbFdasLbzpg0YDj0',
+					'redirect_uri' => 'http://localhost:8080',
+					'code' => $_REQUEST['code']
+				]
+			]);
+			curl_setopt($curl_status, CURLOPT_FRESH_CONNECT, TRUE);
+			$result = curl_exec($curl_status);
+			curl_close($curl_status);
+			$hasil = json_decode($result); //hasil json untuk token
+			$token = $hasil->access_token;
+
+			if (!isset($token))
+				return redirect()->to('/');
+
+			$authorization = "Authorization: Bearer " . $token;
+
+			$curl_status = curl_init();
+
+			curl_setopt_array($curl_status, [
+				CURLOPT_RETURNTRANSFER => 1,
+				CURLOPT_URL => 'https://ws.stis.ac.id/api/user',
+				CURLOPT_HTTPHEADER => array($authorization)
+			]);
+
+			curl_setopt($curl_status, CURLOPT_FRESH_CONNECT, TRUE);
+
+			$result = curl_exec($curl_status);
+			curl_close($curl_status);
+
+			// echo ($result);	//cek result sipadu
+			// die();
+
+			$hasil = json_decode($result, true);	// hasil akhir sipadu
+
+			if (isset($hasil['profile']['nim'])) {
+				if ($this->modelAuth->getUserByUsername($hasil['profile']['nim']) == NULL) {
+					$data = array(
+						"username" => $hasil['profile']['nim'],
+						"nama" => $hasil['profile']['nama']
+					);
+					$this->modelAuth->insertUser($data);
+				}
+
+				$user = $this->modelAuth->getUserByUsername($hasil['profile']['nim']);
+
+				session()->set([
+					'id_user' => $user['id'],
+					'username' => $user['username'],
+					'nama' => $user['nama'],
+					'role' => $user['role']
+				]);
+			} else {
+				if ($this->modelAuth->getUserByUsername($hasil['profile']['username']) == NULL) {
+					$data = array(
+						"username" => $hasil['profile']['username'],
+						"nama" => $hasil['profile']['nama']
+					);
+					$this->modelAuth->insertUser($data);
+				}
+
+				$user = $this->modelAuth->getUserByUsername($hasil['profile']['username']);
+
+				session()->set([
+					'id_user' => $user['id'],
+					'username' => $user['username'],
+					'nama' => $user['nama'],
+					'role' => $user['role']
+				]);
+			}
+
+			setcookie('login', 'yes', time() + 60, $_SERVER['SERVER_NAME']);
+
+			echo '<script>window.close();</script>';
+
+			session()->setFlashdata('pesan', 'Login berhasil. Hai, <b>' . session('username') . '!</b>');
+			session()->setFlashdata('warna', 'success');
+			die();
+		}
+
+		$data = [
+			'title' => 'Dashboard | Riset 5 Website Alumni',
 		];
 		return view('pages/dashboard', $data);
 	}
@@ -23,10 +121,11 @@ class Pages extends BaseController
 
 	public function login()
 	{
-        $data = [
-            'title' => 'Login | Riset 5 Website Alumni',
+		$data = [
+			'title' 	=> 'Login | Riset 5 Website Alumni',
+			'template'	=> 'login',
 		];
-		return view('pages/login', $data);
+		return view('login/login', $data);
 	}
 
 	//--------------------------------------------------------------------
@@ -34,12 +133,8 @@ class Pages extends BaseController
 
 	public function userInfo()
 	{
-		$info = $this->userModel->where(['nama'=> 'mochi'])->first(); //ini bakal diganti sama indentify si user yang login
-        $data = [
+		$data = [
 			'title' => 'Profile | Riset 5 Website Alumni',
-			'nama' 		=> $info['nama'],
-			'nim' 		=> $info['nim'],
-			'angkatan'	=> $info['angkatan'],
 		];
 
 		return view('pages/userinfo', $data);
@@ -47,4 +142,4 @@ class Pages extends BaseController
 
 	//--------------------------------------------------------------------
 
-}
+} -->
