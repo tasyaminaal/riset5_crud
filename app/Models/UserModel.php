b73a2cf5e75293e5eb292aaac5cd4219b2a157b3
Xeryus <akhmadfadilm@gmail.com>
2020-12-24 09:31:28 +0700

    menambah user (model+migration)

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -1,13 +1,42 @@
-<?php namespace App\Models;
+<?php
+
+namespace App\Models;
 
 use CodeIgniter\Model;
 
 class UserModel extends Model
 {
-    protected $table      = 'user';
-    protected $primaryKey = 'id';
+    protected $table = 'user';
+    protected $allowedFields = ['id', 'nama', 'email', 'nip'];
 
-    protected $useTimestamps = true;
-    protected $createdField  = 'created_at';
-    protected $updatedField  = 'updated_at';
+
+    public function getAll()
+    {
+        return $this->db->table('user')->get()->getResultArray();
+    }
+
+    public function jumlahUser()
+    {
+        return $this->db->table('user')->select('*')->countAllResults();
+    }
+
+    public function hapusUser($nip)
+    {
+        $this->db->table('user')->delete(['nip' => $nip]);
+    }
+
+    public function insertUser($data)
+    {
+        $this->db->table('user')->insert($data);
+    }
+
+    public function getUserByNIP($nip)
+    {
+        return $this->builder()->where('nip', $nip)->get()->getFirstRow('array');
+    }
+
+    public function getUserByEmail($email)
+    {
+        return $this->builder()->where('email', $email)->get()->getFirstRow('array');
+    }
 }
