b2c46571e43797d53f11a6bd9fab1535f9c9abc4
Elanuzul <74336615+Elanuzul@users.noreply.github.com>
2020-12-26 10:52:21 +0700

    manual - modif resetpass.php

--- app/Views/reset/resetpass.php
+++ app/Views/reset/resetpass.php
@@ -7,29 +7,52 @@
 
         <div class="text-xl uppercase font-bold mb-8 text-center">RESET PASSWORD</div>
 
+        <?= view('Myth\Auth\Views\_message_block') ?>
+
+        <p><?= lang('Auth.enterCodeEmailPassword') ?></p>
+
         <div class=" text-left w-11/12 md:w-9/12 ">
-            <form action="">
+            <form action="<?= route_to('reset-password') ?>" method="post">
+                <?= csrf_field() ?>
                 <div class="flex flex-col items-center">
 
-                    <div class="flex items-center my-3 w-full mb-4">
-                        <label for="emailAdd" class="my-auto text-sm md:text-base w-32 md:w-auto ">E-mail Address&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:</label>
-                        <input type="text" id="emailAdd" name="emailAdd" class="shadow-md border-2 py-2 px-3 border-gray-200 rounded-md ml-1 md:ml-2 flex-grow focus:border-gray-700 focus:outline-none placeholder-gray-700  text-xs md:text-base align-middle my-auto" placeholder="type your e-mail address" required>
+                    <div class="form-group flex items-center my-3 w-full mb-4">
+                        <label for="token" class="my-auto text-sm md:text-base w-32 md:w-auto "><?= lang('Auth.token') ?>:</label>
+                        <input type="text" id="token" name="token" class="shadow-md border-2 py-2 px-3 border-gray-200 rounded-md ml-1 md:ml-2 flex-grow focus:border-gray-700 focus:outline-none placeholder-gray-700  text-xs md:text-base align-middle my-auto <?php if (session('errors.token')) : ?>is-invalid<?php endif ?>>" placeholder="<?= lang('Auth.token') ?>" value="<?= old('token', $token ?? '') ?>">
+                        <div class="invalid-feedback">
+                            <?= session('errors.token') ?>
+                        </div>
                     </div>
 
-                    <div class="flex items-center my-3 w-full mb-4">
-                        <label for="newpass" class="my-auto text-sm md:text-base w-32 md:w-auto ">New Password&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:</label>
-                        <input type="text" id="newpass" name="newpass" class="shadow-md border-2 py-2 px-3 border-gray-200 rounded-md ml-1 md:ml-2 flex-grow focus:border-gray-700 focus:outline-none placeholder-gray-700  text-xs md:text-base align-middle my-auto" placeholder="type your new password" required>
+
+                    <div class="form-group flex items-center my-3 w-full mb-4">
+                        <label for="email" class="my-auto text-sm md:text-base w-32 md:w-auto "><?= lang('Auth.email') ?>:</label>
+                        <input type="email" id="email" name="email" class="shadow-md border-2 py-2 px-3 border-gray-200 rounded-md ml-1 md:ml-2 flex-grow focus:border-gray-700 focus:outline-none placeholder-gray-700  text-xs md:text-base align-middle my-auto <?php if (session('errors.email')) : ?>is-invalid<?php endif ?>" aria-describedby="emailHelp" placeholder="<?= lang('Auth.email') ?>" value="<?= old('email') ?>">
+                        <div class="invalid-feedback">
+                            <?= session('errors.email') ?>
+                        </div>
                     </div>
 
-                    <div class="flex items-center my-3 w-full mb-8">
-                        <label for="confirmpass" class="my-auto text-sm md:text-base  w-32 md:w-auto ">Confirm Password&nbsp;:</label>
-                        <input type="text" id="confirmpass" name="confirmpass" class="shadow-md border-2 py-2 px-3 border-gray-200 rounded-md ml-1 md:ml-2 flex-grow focus:border-gray-700 focus:outline-none placeholder-gray-700  text-xs md:text-base align-middle my-auto" placeholder="confirm your password" required>
+                    <div class="form-group flex items-center my-3 w-full mb-4">
+                        <label for="password" class="my-auto text-sm md:text-base w-32 md:w-auto "><?= lang('Auth.newPassword') ?>:</label>
+                        <input type="password" id="password" name="password" class="shadow-md border-2 py-2 px-3 border-gray-200 rounded-md ml-1 md:ml-2 flex-grow focus:border-gray-700 focus:outline-none placeholder-gray-700  text-xs md:text-base align-middle my-auto <?php if (session('errors.password')) : ?>is-invalid<?php endif ?>" placeholder="type your new password">
+                        <div class="invalid-feedback">
+                            <?= session('errors.password') ?>
+                        </div>
+                    </div>
+
+                    <div class="form-group flex items-center my-3 w-full mb-8">
+                        <label for="pass_confirm" class="my-auto text-sm md:text-base  w-32 md:w-auto "><?= lang('Auth.newPasswordRepeat') ?>:</label>
+                        <input type="password" id="pass_confirm" name="pass_confirm" class="shadow-md border-2 py-2 px-3 border-gray-200 rounded-md ml-1 md:ml-2 flex-grow focus:border-gray-700 focus:outline-none placeholder-gray-700  text-xs md:text-base align-middle my-auto" placeholder="confirm your password">
+                        <div class="invalid-feedback">
+                            <?= session('errors.pass_confirm') ?>
+                        </div>
                     </div>
 
                 </div>
 
                 <div class="flex justify-center w-full">
-                    <input type="submit" value="Send Password Reset Link" class="text-xs my-auto text-white font-bold hover:from-blue-500 hover:to-indigo-600  cursor-pointer focus:outline-none bg-gradient-to-r from-blue-500 to-indigo-500 py-2 px-2 md:px-4 ml-2 rounded-md">
+                    <button type="submit" class="text-xs my-auto text-white font-bold hover:from-blue-500 hover:to-indigo-600  cursor-pointer focus:outline-none bg-gradient-to-r from-blue-500 to-indigo-500 py-2 px-2 md:px-4 ml-2 rounded-md"><?= lang('Auth.resetPassword') ?></button>
                 </div>
 
             </form>
