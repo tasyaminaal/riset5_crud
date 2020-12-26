c58c0de341d47d584ceeb58e45aec4f09ebc5268
Elanuzul <74336615+Elanuzul@users.noreply.github.com>
2020-12-26 10:44:55 +0700

    manual - modif User.php

--- app/Controllers/User.php
+++ app/Controllers/User.php
@@ -6,6 +6,13 @@ class User extends BaseController
 {
 	public function index() //user detail + form update
 	{
+		$data = [
+			'title' => 'User | Riset 5 Website Alumni',
+		];
+		$users = new \Myth\Auth\Models\UserModel();
+		$data['users'] = $users->findAll();
+
+		return view('pages/user', $data);
 	}
 
 	//--------------------------------------------------------------------
