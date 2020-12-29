46655fc3b35e0751cceb79ebfb44a2ae64bf444d
Xeryus <akhmadfadilm@gmail.com>
2020-12-29 17:43:52 +0700

    fix migration & seed

--- app/Database/Seeds/AlumniSeeder.php
+++ app/Database/Seeds/AlumniSeeder.php
@@ -1,36 +1,38 @@
-<?php namespace App\Database\Seeds;
+<?php
+
+namespace App\Database\Seeds;
 
 class AlumniSeeder extends \CodeIgniter\Database\Seeder
 {
         public function run()
         {
-            $faker = \Faker\Factory::create('id_ID');
-            for($i=0; $i<100;$i++){
-                $data = [
-                        'angkatan'      => $faker->numberBetween($min = 1, $max = 62),
-                        'nama'          => $faker->name,
-                        'nim'           => $faker->randomNumber($nbDigits = 9, $strict = false),
-                        'jenisKelamin'  => $faker->randomElement($array = array ('L','P')),
-                        'tempatLahir'   => $faker->city,
-                        'tanggalLahir'  => $faker->date($format = 'Y-m-d', $max = 'now'),
-                        'telpAlumni'    => $faker->phoneNumber,
-                        'alamat'        => $faker->address,
-                        'statusBekerja' => $faker->boolean,
-                        'perkiraanPensiun' => $faker->year,
-                        'jabatanTerakhir'  => $faker->jobTitle,
-                        'aktifPNS'      => $faker->boolean,
-                ];
-                $this->db->table('alumni')->insert($data);
-            }
+                $faker = \Faker\Factory::create('id_ID');
+                for ($i = 0; $i < 100; $i++) {
+                        $data = [
+                                'angkatan'      => $faker->numberBetween($min = 1, $max = 62),
+                                'nama'          => $faker->name,
+                                'nim'           => $faker->randomNumber($nbDigits = 9, $strict = false),
+                                'jenisKelamin'  => $faker->randomElement($array = array('L', 'P')),
+                                'tempatLahir'   => $faker->city,
+                                'tanggalLahir'  => $faker->date($format = 'Y-m-d', $max = 'now'),
+                                'telpAlumni'    => $faker->phoneNumber,
+                                'alamat'        => $faker->address,
+                                'statusBekerja' => $faker->boolean,
+                                'perkiraanPensiun' => $faker->year,
+                                'jabatanTerakhir'  => $faker->jobTitle,
+                                'aktifPNS'      => $faker->boolean
+                        ];
+                        $this->db->table('alumni')->insert($data);
+                }
 
-                    // // Simple Queries
-                    // $this->db->query("INSERT INTO users (username, email) VALUES(:username:, :email:)",
-                    //         $data
-                    // );
+                // // Simple Queries
+                // $this->db->query("INSERT INTO users (username, email) VALUES(:username:, :email:)",
+                //         $data
+                // );
 
-                    // Using Query Builder
+                // Using Query Builder
 
-                    //banyak data
-                    // $this->db->table('alumni')->insertBatch($data);
+                //banyak data
+                // $this->db->table('alumni')->insertBatch($data);
         }
 }
---
+++ app/Database/Seeds/LoginManualSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+use CodeIgniter\I18n\Time;
+
+class LoginManualSeeder extends Seeder
+{
+	public function run()
+	{
+		$data = [
+			'email'				=> 'elanuzul4@gmail.com',
+			'username'			=> '421044167',
+			'password_hash'		=> '$2y$10$yLFu3bK0s5cHqd1VLT6Eh.GjA3H2GJzwqb6o/gjrhKXTWGkMsh3IS',
+			'reset_at'			=> Time::now(),
+			'active'			=> 1,
+			'force_pass_reset'	=> 0,
+			'created_at'		=> Time::now(),
+			'updated_at'		=> Time::now()
+		];
+		$this->db->table('users')->insert($data);
+	}
+}
